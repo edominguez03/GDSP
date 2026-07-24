8eb51082c43f69a96786ec32f87b3b40ae3942a9
Edgardo Dominguez <dominguezedgardojj@gmail.comgit config --global user.name Edgardo>
2026-07-24 19:39:13 -0400

    Add Lesson 11: While loops

---
+++ 01_python_fundamentals/lesson11.py
@@ -0,0 +1,27 @@
+logged_in = input("Are you logged in? ")
+
+while logged_in == "no":
+    print("Please log in.")
+    logged_in = input("Are you logged in? ") 
+print("Welcome!")
+   
+   
+        
+
+   
+    
+
+
+   
+        
+    
+
+   
+    
+
+    
+
+
+    
+
+  
