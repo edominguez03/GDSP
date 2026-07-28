dc72ef540c11665e23a4a035f248da21442af418
Edgardo Dominguez <dominguezedgardojj@gmail.comgit config --global user.name Edgardo>
2026-07-28 12:35:32 -0400

    Add Lesson 13: Lists

---
+++ 01_python_fundamentals/lesson13.py
@@ -0,0 +1,14 @@
+languages = ["English", "Spanish", "Russian", "Python"]
+
+print(languages)
+print(languages[0])
+print(languages[3])
+print()
+
+print(languages[0])
+print()
+print(languages[3])
+print()
+
+for n in languages:
+    print(n)
