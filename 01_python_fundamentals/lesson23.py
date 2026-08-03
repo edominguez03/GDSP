2e16facfd4a6215836c039dd5675ea0a4e6ae871
Edgardo Dominguez <dominguezedgardojj@gmail.comgit config --global user.name Edgardo>
2026-08-03 10:22:15 -0400

    Add Lesson 23: Keyword arguments

---
+++ 01_python_fundamentals/lesson23.py
@@ -0,0 +1,6 @@
+def book(title, author):
+    return title + " was written by " + author
+
+print(book("Python Crash Course", "Eric Matthes"))
+
+print(book(author ="Eric Matthes", title ="Python Crash Course"))
