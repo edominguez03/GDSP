04f76b97942af52a50cecb6d1839e2226858ab6e
Edgardo Dominguez <dominguezedgardojj@gmail.comgit config --global user.name Edgardo>
2026-08-05 13:23:54 -0400

    Add Lesson 26: Searching inside strings

---
+++ 01_python_fundamentals/lesson26.py
@@ -0,0 +1,9 @@
+filename = "lesson26.py"
+
+print(filename.startswith("lesson"))
+print(filename.endswith(".py"))
+
+sentence = "I love Python"
+
+print(sentence.find("Python"))
+print(sentence.find("Java"))
