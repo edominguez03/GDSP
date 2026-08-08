d1251d49bdf5185afa049eee42c1d874cecee658
Edgardo Dominguez <dominguezedgardojj@gmail.comgit config --global user.name Edgardo>
2026-08-08 19:40:55 -0400

    Add Lesson 27: Dictionaries

---
+++ 01_python_fundamentals/lesson27.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Edgardo",
+    "country": "Chile",
+    "language": "Python",
+    "age": 61
+}
+
+print(student["name"])
+print(student["country"])
+print(student["language"])
+print(student["age"])
