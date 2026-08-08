9ea58ecaa775615ad4317a9dc22129ffbff38713
Edgardo Dominguez <dominguezedgardojj@gmail.comgit config --global user.name Edgardo>
2026-08-08 19:41:37 -0400

    Add Lesson 28: Modifying dictionaries

---
+++ 01_python_fundamentals/lesson28.py
@@ -0,0 +1,11 @@
+student = {
+    "name": "Edgardo",
+    "country": "Chile",
+    "language": "Python"
+}
+
+student["city"] = "Santiago"
+student["language"] = "Russian"
+student["age"] = 61
+
+print(student)
